7f718d5770ad5fa1fece5d5404326fd15827847a
Jack Minchin <jminchin@oxfordeconomics.com>
2022-02-23 10:02:34 +0000

    latest

--- testyles/styles.py
+++ testyles/styles.py
@@ -23,8 +23,11 @@ def init_styles(styleSheet = mainStyles):
 def subplots(nrows=1, ncols=1, sharex=False, sharey=False, squeeze=True, subplot_kw=None, gridspec_kw=None, **fig_kw):
     fig, axes = plt.subplots(nrows, ncols, sharex=sharex, sharey=sharey, squeeze=squeeze, subplot_kw=subplot_kw, gridspec_kw=gridspec_kw, **fig_kw)
 
-    plt.xlabel('Source: Tourism Economics', wrap=True, horizontalalignment='left', fontsize=8, x=0)
     return fig, axes
+
+def add_source(ax, source='Tourism Economics', yoffset=-20):
+    ax.annotate('Source: {}'.format(source), (0,0), (0, yoffset), fontsize=6, 
+             xycoords='axes fraction', textcoords='offset points', va='top')
 
 def clear_font_cache():
     if (os.path.exists(mpl.get_cachedir())):
